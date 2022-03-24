9c1aef6b523d2153db450686c44c7276bef59d14
Dami Park <ekal0323@gmail.com>
2022-03-24 19:51:22 +0900

    마이페이지 생성 및 내부 버튼 화면 구현

--- app/src/main/java/com/techtown/matchingservice/LoginActivity.kt
+++ app/src/main/java/com/techtown/matchingservice/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.techtown.matchingservice
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
