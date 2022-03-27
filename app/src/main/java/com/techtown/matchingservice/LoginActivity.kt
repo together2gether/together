ad4567eaeae5dc6857d04c22b816cc4039abd057
OkHui Cheon <dnflwlq0378@naver.com>
2022-03-27 15:06:54 +0900

    구글 로그인 기능 구현

---
+++ app/src/main/java/com/techtown/matchingservice/LoginActivity.kt
@@ -0,0 +1,76 @@
+package com.techtown.matchingservice
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.techtown.matchingservice.MainActivity
+import com.techtown.matchingservice.R
+import com.techtown.matchingservice.databinding.LoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: LoginBinding
+    lateinit var googleSigninClient : GoogleSignInClient
+    val TAG = "GOOGLE_LOGIN"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.login)
+        auth = FirebaseAuth.getInstance()
+        binding.googleSigninButton.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("372316850033-9lvhsfl1ea12mks4rdgl69rk3s3ulbmb.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSigninClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun googleLogin() {
+        val signInIntent = googleSigninClient.signInIntent
+        googleLoginResult.launch(signInIntent)
+    }
+
+
+
+    fun firebaseAuthWithGoogle(idToken: String?) {
+        var credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                if (auth.currentUser!!.isEmailVerified) {
+                    //이메일 인증이 되었을때
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        // 구글 로그인이 성공했을때 이메일 값이 넘어오는데 -> Token -> Email -> Firebase 서버
+        var data = result.data
+        var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+        firebaseAuthWithGoogle(account.idToken)
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
