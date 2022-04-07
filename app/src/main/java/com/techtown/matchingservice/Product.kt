aa6bf98a3c96b94978c9a0284b167b0f357eeec2
bin000527 <bin000527@sookmyung.ac.kr>
2022-04-07 20:19:28 +0900

    채팅 중간 업뎃

--- app/src/main/java/com/techtown/matchingservice/Product.kt
+++ app/src/main/java/com/techtown/matchingservice/Product.kt
@@ -1,17 +1,12 @@
 package com.techtown.matchingservice
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.FirebaseFirestore
 import com.techtown.matchingservice.databinding.ProductInfoBinding
-import com.techtown.matchingservice.model.ContentDTO
-
 
 class Product : AppCompatActivity() {
     private lateinit var binding: ProductInfoBinding
@@ -34,35 +29,10 @@ class Product : AppCompatActivity() {
         binding.productInfoPlace.text = intent.getStringExtra("place").toString()
         binding.productInfoCycle.text = intent.getStringExtra("cycle").toString()
         binding.productInfoParticipationNumber.text = intent.getStringExtra("participationCount").toString()+" / "+intent.getStringExtra("participationTotal").toString()
-        if(intent.getStringExtra("uidkey").toString()=="true"){
-            binding.productInfoParticipation.isEnabled=false
-        }
+
         binding.productInfoBack.setOnClickListener(){
             finish()
         }
 
-        binding.productInfoParticipation.setOnClickListener(){
-            favoriteEvent()
-        }
-    }
-
-    fun favoriteEvent(){
-        var docId = intent.getStringExtra("id").toString()
-        var tsDoc = firestore?.collection("images")?.document(docId)
-        firestore?.runTransaction {
-                transition ->
-            var contentDTO = transition.get(tsDoc!!).toObject(ContentDTO::class.java)
-            if(contentDTO!!.Participation.containsKey(uid)){
-                binding.productInfoParticipation.isEnabled = false
-            }else{
-                //참여를 누르지 않은 상태 -> 클릭시 참여
-                contentDTO.ParticipationCount = contentDTO.ParticipationCount + 1
-                contentDTO.Participation[uid] = true
-            }
-            transition.set(tsDoc,contentDTO)
-            binding.productInfoParticipationNumber.text=contentDTO.ParticipationCount.toString()+" / "+contentDTO.ParticipationTotal
-        }
-
     }
 }
-
