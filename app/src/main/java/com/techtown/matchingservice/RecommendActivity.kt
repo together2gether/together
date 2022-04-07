3fc5c4f440a53bf10ac914333d2abaffe0fd5d85
bin000527 <bin000527@sookmyung.ac.kr>
2022-04-07 16:36:13 +0900

    쇼핑몰 api 중간 업뎃

--- app/src/main/java/com/techtown/matchingservice/RecommendActivity.kt
+++ app/src/main/java/com/techtown/matchingservice/RecommendActivity.kt
@@ -1,13 +1,23 @@
 package com.techtown.matchingservice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RecommendActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recommend)
+        val btn_search = findViewById<Button>(R.id.btn_search)
+        btn_search.setOnClickListener {
+            var edit_search = findViewById<TextView>(R.id.edit_search)
+            var keyword = edit_search.text.toString()
+            //val intent = Intent(this, )
+
+        }
         val finish = findViewById<Button>(R.id.button50)
         finish.setOnClickListener {
             finish()
---
+++ app/src/main/java/com/techtown/matchingservice/model/ChatModel.kt
@@ -0,0 +1,7 @@
+package com.techtown.matchingservice.model
+
+class ChatModel (val users: HashMap<String, Boolean> = HashMap(),
+val comments : HashMap<String, Comment> = HashMap()
+){
+    class Comment(val uid: String? = null, val message: String? = null, val time: String? = null)
+}
