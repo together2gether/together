70b00127becaeea5646dcf585ec77086b6e17552
Dami Park <ekal0323@gmail.com>
2022-05-19 16:41:20 +0900

    탭1 속도 수정

--- app/src/main/java/com/techtown/matchingservice/Fragment1.kt
+++ app/src/main/java/com/techtown/matchingservice/Fragment1.kt
@@ -6,11 +6,13 @@ import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -39,7 +41,7 @@ class Fragment1 : Fragment() {
     private lateinit var binding: Fragment1Binding
     var firestore: FirebaseFirestore? = null
     lateinit var uid: String
-    var mylat : Double =0.0
+    var mylat : Double = 0.0
     var mylon : Double = 0.0
     var mylocation : String = ""
     lateinit var mycor : List<Address>
@@ -48,9 +50,15 @@ class Fragment1 : Fragment() {
     var items = arrayOf(" 최신 순 ", " 가격 순 ", " 거리 순 ")
     var items_filter = arrayOf(" 1km 이내 ", " 2km 이내 ", " 3km 이내 ")
     var contentList = mutableListOf<Triple<String, ContentDTO, Double>>()
+    var contentList2 = mutableListOf<Triple<String, ContentDTO, Double>>()
     var filteringList = mutableListOf<Triple<String, ContentDTO, Double>>()
     lateinit var geocoder : Geocoder
     var dist =1000
+    var isopen = "close"
+    var item : ContentDTO? = null
+    //var cor : String? = null
+    var lat : Double? = null
+    var lon : Double? = null
 
     //var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
     //var contentUidList: ArrayList<String> = arrayListOf()
@@ -67,7 +75,7 @@ class Fragment1 : Fragment() {
         geocoder = Geocoder(context)
         val userRef = infoRef.child(uid.toString())
 
-        userRef.addValueEventListener(object : ValueEventListener{
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener{
             override fun onCancelled(error: DatabaseError) {
 
             }
@@ -75,14 +83,42 @@ class Fragment1 : Fragment() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 var userInfo = snapshot.getValue<UsersInfo>()
                 mylocation = userInfo!!.address.toString()
+                Log.e("mylocation", mylocation!!)
                 mycor = geocoder.getFromLocationName(mylocation,1)
                 mylat = mycor[0].latitude
                 mylon = mycor[0].longitude
+                Log.e("mylat", mylat.toString() + ", " + mylon.toString())
+                firestore?.collection("images")
+                    ?.orderBy("timestamp")
+                    ?.addSnapshotListener { value, error ->
+                        //contentDTOs.clear()
+                        //contentUidList.clear()
+
+                        contentList2.clear()
+                        if (value?.documents != null) {
+                            //LoadingDialog(requireContext()).show()
+                            for (snapshot in value!!.documents) {
+                                item = snapshot.toObject(ContentDTO::class.java)
+                                var cor = geocoder.getFromLocationName(item!!.place.toString(), 1)
+                                lat = cor[0].latitude
+                                lon = cor[0].longitude
+                                var distance = Fragment2.DistanceManager.getDistance(mylat!!, mylon!!, lat!!, lon!!).toDouble()
+                                /*if (distance <= 3000!!.toInt()) {
+                                    //contentDTOs.add(item!!)
+                                    //contentUidList.add(snapshot.id)
+                                    contentList.add(Triple(snapshot.id, item, distance))
+                                }*/
+                                contentList2.add(Triple(snapshot.id, item, distance) as Triple<String, ContentDTO, Double>)
+                                Log.e("product my loc", mylat.toString() + ", " + mylon.toString())
+                                Log.e("product loc", lat.toString() + ", " + lon.toString())
+                                Log.e("product", item!!.place + ", " + distance.toString())
+                            }
+                            //LoadingDialog(requireContext()).dismiss()
+                        }
+                    }
             }
 
         })
-        //LoadingDialog(requireContext()).show()
-        //LoadingDialog(requireContext()).dismiss()
         firestore?.collection("images")
             ?.orderBy("timestamp")
             ?.addSnapshotListener { value, error ->
@@ -93,23 +129,28 @@ class Fragment1 : Fragment() {
                 if (value?.documents != null) {
                     //LoadingDialog(requireContext()).show()
                     for (snapshot in value!!.documents) {
-                        var item = snapshot.toObject(ContentDTO::class.java)
-                        var cor = geocoder.getFromLocationName(item!!.place.toString(), 1)
-                        var lat = cor[0].latitude
-                        var lon = cor[0].longitude
-                        var distance =
-                            Fragment2.DistanceManager.getDistance(mylat, mylon, lat, lon).toDouble()
+                        item = snapshot.toObject(ContentDTO::class.java)
+
                         /*if (distance <= 3000!!.toInt()) {
                             //contentDTOs.add(item!!)
                             //contentUidList.add(snapshot.id)
                             contentList.add(Triple(snapshot.id, item, distance))
                         }*/
-                        contentList.add(Triple(snapshot.id, item, distance))
+                        contentList.add(Triple(snapshot.id, item, null) as Triple<String, ContentDTO, Double>)
+                        Log.e("product my loc", mylat.toString() + ", " + mylon.toString())
+                        Log.e("product loc", lat.toString() + ", " + lon.toString())
+                        //Log.e("product", item!!.place + ", " + distance.toString())
                     }
                     //LoadingDialog(requireContext()).dismiss()
                 }
             }
 
+
+
+
+
+        //LoadingDialog(requireContext()).show()
+        //LoadingDialog(requireContext()).dismiss()
 
         //LoadingDialog(requireContext()).dismiss()
         binding.fragment1ProductRegistration.setOnClickListener {
@@ -124,10 +165,12 @@ class Fragment1 : Fragment() {
         binding.menu.setOnFloatingActionsMenuUpdateListener(object: FloatingActionsMenu.OnFloatingActionsMenuUpdateListener{
             override fun onMenuExpanded() {
                 binding.background.setBackgroundColor(Color.parseColor("#80000000"))
+                isopen = "open"
             }
 
             override fun onMenuCollapsed() {
                 binding.background.setBackgroundColor(Color.parseColor("#00000000"))
+                isopen = "close"
             }
         })
 
@@ -171,8 +214,8 @@ class Fragment1 : Fragment() {
                         contentList.sortBy { it.second.price / it.second.ParticipationTotal }
                     }
                     2->{
-                        contentList.sortBy { it.third }
-                        contentList.reverse()
+                        contentList2.sortBy { it.third }
+                        contentList = contentList2
                     }
                 }
                 binding.fragment1RecyclerView.adapter!!.notifyDataSetChanged()
@@ -218,6 +261,7 @@ class Fragment1 : Fragment() {
         return binding.root
 
     }
+
     fun filtering(dist : Int? = 1000){
         filteringList.clear()
         for(i in contentList){
@@ -261,38 +305,41 @@ class Fragment1 : Fragment() {
             var timeLong : Long? = contentList[position].second.timestamp
             viewHolder.textTime.text = timeDiff(timeLong!!)
 
-
             //viewHolder.productitemParticipation.text =
             //    "현재 " + participationCount + " / " + contentDTOs[position].ParticipationTotal.toString()
             //click
             viewHolder.productitemCardView.setOnClickListener {
+                if(isopen == "open"){
 
-                Intent(context, Product::class.java).apply {
-                    putExtra("position", position.toString())
-                    putExtra("product", contentList[position].second.product)
-                    putExtra("imageUrl", contentList[position].second.imageUrl)
-                    putExtra("price", contentList[position].second.price.toString())
-                    putExtra("totalNumber", contentList[position].second.totalNumber.toString())
-                    putExtra("cycle", contentList[position].second.cycle.toString())
-                    putExtra("unit", contentList[position].second.unit.toString())
-                    putExtra("URL", contentList[position].second.url)
-                    putExtra("place", contentList[position].second.place)
-                    putExtra("timestamp", contentList[position].second.timestamp.toString())
-                    putExtra("participationCount", participationCount)
-                    putExtra("id", contentList[position].first)
-                    putExtra("position", position.toString())
-                    putExtra(
-                        "uidkey",
-                        contentList[position].second.Participation.containsKey(uid).toString()
-                    )
-                    putExtra(
-                        "participationTotal",
-                        contentList[position].second.ParticipationTotal.toString()
-                    )
-                    //putExtra("id", contentUidList[position])
-                    putExtra("Uid", contentList[position].second.uid.toString())
+                }else{
+                    Intent(context, Product::class.java).apply {
+                        putExtra("position", position.toString())
+                        putExtra("product", contentList[position].second.product)
+                        putExtra("imageUrl", contentList[position].second.imageUrl)
+                        putExtra("price", contentList[position].second.price.toString())
+                        putExtra("totalNumber", contentList[position].second.totalNumber.toString())
+                        putExtra("cycle", contentList[position].second.cycle.toString())
+                        putExtra("unit", contentList[position].second.unit.toString())
+                        putExtra("URL", contentList[position].second.url)
+                        putExtra("place", contentList[position].second.place)
+                        putExtra("timestamp", contentList[position].second.timestamp.toString())
+                        putExtra("participationCount", participationCount)
+                        putExtra("id", contentList[position].first)
+                        putExtra("position", position.toString())
+                        putExtra(
+                            "uidkey",
+                            contentList[position].second.Participation.containsKey(uid).toString()
+                        )
+                        putExtra(
+                            "participationTotal",
+                            contentList[position].second.ParticipationTotal.toString()
+                        )
+                        //putExtra("id", contentUidList[position])
+                        putExtra("Uid", contentList[position].second.uid.toString())
 
-                }.run { context?.startActivity(this) }
+                    }.run { context?.startActivity(this) }
+                }
+
             }
 
 
